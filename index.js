ddb4b90a418c5fa0a937b2c9b3b20754f1e9e645
madhu-software <inaparthi.madhukiran.s89@kalvium.community>
2025-03-21 11:41:26 +0530

    done

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
+const mongoose = require('mongoose');
 const express = require('express');
 const { resolve } = require('path');
 
 const app = express();
-const port = 3010;
 
 app.use(express.static('static'));
+
+require('dotenv').config();
+
+const port = process.env.PORT;
+const mongoUri = process.env.MONGO_URI;
+
+if (!port || !mongoUri) {
+  console.error('Missing PORT or MONGO_URI in environment variables');
+  process.exit(1);
+}
+
+mongoose.connect(mongoUri)
+    .then(() => console.log('Connected to database'))
+    .catch((err) => console.error('Error connecting to database', err));
 
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
